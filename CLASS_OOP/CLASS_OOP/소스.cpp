9a53acecdde2832b3026470785890df46c04c0b1
ohyoungkyu <come8908@gmail.com>
2021-09-13 17:18:05 +0900

    객체지향(oop) , 생성자/소멸자 -ing

---
+++ CLASS_OOP/CLASS_OOP/소스.cpp
@@ -0,0 +1,22 @@
+﻿#include <cstdio>
+
+// 회사의 erp시스템
+// 나이, 연봉, 이름, 혈액형, 이메일주소, 주민번호, 3대(벤치,데드,스쿼트), 결혼여부, 자녀유무, 유튜브 닉네임
+// 나이, 연봉, 이름, 이메일주소, 주민번호, 결혼여부, 자녀유무
+
+struct Person {
+	int age;
+	float salary;
+	char name[100];
+
+	void goTowork() {};
+	void workoff() {};
+	void vacation() {};
+	//void gameInHome() {};
+}; // 추상화(abstraction)
+
+int main() {
+
+	return 0;
+
+}
