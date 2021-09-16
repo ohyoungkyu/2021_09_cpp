69a8c79c9fcbafa0541f6a96595012029cc90ba3
ohyoungkyu <come8908@gmail.com>
2021-09-16 10:50:05 +0900

    상속_접근지정자

---
+++ Inherltance_AccessModlfler/Inherltance_AccessModlfler/소스1.cpp
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+
+class character {
+public:
+	int stamina;
+	int attack;
+	int range;
+	int speed;
+
+	character() {
+
+	}
+};
+
+class goblin : public character {
+public :
+
+};
