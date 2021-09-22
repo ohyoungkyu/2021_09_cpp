6eed4d837641e9ca29eb8f190fc1eadb0fb31a16
ohyoungkyu <come8908@gmail.com>
2021-09-22 15:07:22 +0900

    상속 연습문제 GameCharacter

--- Inherltance_AccessModlfler/Inherltance_AccessModlfler/소스1.cpp
+++ Inherltance_AccessModlfler/Inherltance_AccessModlfler/소스1.cpp
@@ -1,22 +1,35 @@
-#define _CRT_SECURE_NO_WARNINGS
+﻿#include "헤더1.h"
 
-#include <cstdio>
-#include <cstring>
-#include <cstdlib>
+int main() {
 
-class character {
-public:
-	int stamina;
-	int attack;
-	int range;
-	int speed;
+	Unit* units[10];
 
-	character() {
+	srand(time(0));
 
+	for (int i = 0; i < 10; i++) {
+		switch (rand() % 4) {
+		case 0:
+			units[i] = new Goblin();
+			break;
+		case 1:
+			units[i] = new Orc();
+			break;
+		case 2:
+			units[i] = new Slime();
+			break;
+		case 3:
+			units[i] = new SkeletonArcher();
+			break;
+		}
 	}
-};
 
-class goblin : public character {
-public :
+	for (int i = 0; i < 10; i++) {
+		units[i]->printInfo();
+	}
+	for (int i = 0; i < 10; i++) {
+		delete units[i];
+	}
 
-};
+	return 0;
+
+}
