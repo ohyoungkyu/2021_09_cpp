b997f7a1744ad94cca9177058b2bd35af0bbbac1
ohyoungkyu <come8908@gmail.com>
2021-10-04 18:19:46 +0900

    String And NameSpace

---
+++ String And NameSpace/String And NameSpace/소스.cpp
@@ -0,0 +1,21 @@
+﻿//문자열
+#define _CRT_SECURE_NO_WARNINGS
+//strcpy strcat
+
+#include <cstdio>
+//#include <cstring>
+#include <string>
+
+int main() {
+
+	char myString[100];
+
+	strcpy(myString, "Hello World!\n");
+
+	strcat(myString, "Hello C++!");
+
+	printf("%s", myString);
+
+	return 0;
+
+}
